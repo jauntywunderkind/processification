2120874f013e224029b19b9917e5fc432e01fda3
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-09-27 14:31:57 -0400

    show that async-lift can be done statically

--- .test/async-lift.js
+++ .test/async-lift.js
@@ -1,9 +1,9 @@
 "use module"
 import tape from "tape"
-import promiseInstantiator from "../promise-instantiator.js"
+import AsyncLift from "../async-lift.js"
 import delay from "delay"
 
-tape( "test promise-instantiator", async function( t){
+tape( "test lifted async", async function( t){
 	async function Rolander( res, rej, self, greeting){
 		self.ourName= "roland"
 		res( "hello")
@@ -12,8 +12,8 @@ tape( "test promise-instantiator", async function( t){
 	}
 
 	const
-	  instantiator= promiseInstantiator( Rolander),
-	  p= instantiator( "heyya")
+	  lifted= AsyncLift( Rolander),
+	  p= lifted( "heyya")
 
 	// check base promise behavior
 	t.equal( p.ourName, "roland", "constructor had a this context")
@@ -25,24 +25,52 @@ tape( "test promise-instantiator", async function( t){
 	await delay( 10)
 	t.equal( p.greeting, "heyya", "and now has ran")
 
-	// test some props of the instantiator
+	// test some props of the lifted
 	t.equal( p.constructor.name, "RolanderPromise", "instantiated has right class")
-	t.equal( instantiator.class.name, "RolanderPromise", "instantiator knows class")
+	t.equal( lifted.class.name, "RolanderPromise", "lifted knows class")
 	t.end()
 })
 
-tape( "test promise-instantiator-ed field", async function( t){
+tape( "test lifted async class-field", async function( t){
 	class Foo{
 		color= "red"
 		constructor(){
-			this.bar= promiseInstantiator( this.bar)
+			this.bar= AsyncLift( this.bar)
 		}
+		// here is the magic: additional parameters passed in to bar in the lifted form
 		async bar( res, rej, self, greeting= "sup?"){
 			this.color= "green" // this is still this
 			self.ourName= "roland" // self is our promise instance
 			res( "hello")
 		}
 	}
+
+	const foo= new Foo()
+	t.equal( foo.color, "red", "foo's color starts red")
+
+	// run
+	const b= foo.bar()
+	t.equal( foo.color, "green", "foo's color goes green")
+	t.equal( b.ourName, "roland", "promise has ourName")
+
+	// post run
+	const bv= await b
+	t.equal( bv, "hello", "resolves as usual")
+	t.end()
+})
+
+tape( "test statically lifted async class-field", async function( t){
+	class Foo{
+		color= "red"
+		// showing off
+		async bar( res, rej, self, greeting= "sup?"){
+			this.color= "green" // this is still this
+			self.ourName= "roland" // self is our promise instance
+			res( "hello")
+		}
+	}
+	// lift can be done statically! otherwise same test as previous
+	Foo.prototype.bar= AsyncLift( Foo.prototype.bar)
 
 	const foo= new Foo()
 	t.equal( foo.color, "red", "foo's color starts red")
